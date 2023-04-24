5e8efe389662d7eea1e0203f8894b082ad367999
Bruno <bruno_moises_stz@hotmail.com>
2023-04-24 15:14:37 -0300

    style: create style to header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  background-color: ${props => props.theme.colors.secondary};
+  height: 100px;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  font-size: 26px;
+
+  a {
+    position: absolute;
+    left: 25px;
+  }
+`
+
+export const MenuItens = styled.div`
+  display: flex;
+  gap: 80px;
+  align-items: center;
+
+  a {
+    position: relative;
+    
+    :hover {
+      color: ${props => props.theme.colors.tertiary};
+    }
+  }
+`
