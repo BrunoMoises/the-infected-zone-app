fdf03dc561c5c642c3ed868b7f929e1b6b7d04a8
Bruno <bruno_moises_stz@hotmail.com>
2023-04-24 16:39:38 -0300

    feat: create card component

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,33 @@
+import {Container, Menu} from './styles'
+import Image, {StaticImageData} from "next/image";
+import {AiOutlineHeart} from 'react-icons/ai'
+import {FaShare} from 'react-icons/fa'
+import Link from "next/link";
+
+interface cardProps {
+    image: StaticImageData;
+    title: string;
+    id: string;
+}
+
+const Card = ({image, title, id}: cardProps) => {
+    return (
+        <Container>
+            <Link href={`/post/${id}`}>
+                <Image src={image} alt={title} width={205}/>
+                <h1>{title}</h1>
+            </Link>
+            <Menu>
+                <Link href='/like'>
+                    <AiOutlineHeart size={40}/>
+                </Link>
+                <span>2.34k</span>
+                <Link href='/share'>
+                    <FaShare size={40}/>
+                </Link>
+            </Menu>
+        </Container>
+    );
+}
+
+export default Card
