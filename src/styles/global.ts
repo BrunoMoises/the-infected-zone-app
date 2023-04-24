41017c7c9e1d2bb90ccf3c5b15fe3a211a829562
Bruno <bruno_moises_stz@hotmail.com>
2023-04-24 16:39:08 -0300

    style: create style global to input

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,21 +1,23 @@
-import styled, { createGlobalStyle } from 'styled-components'
-
-export const Container = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    gap: 200px;
-`
+import {createGlobalStyle} from 'styled-components'
 
 export default createGlobalStyle`
-    * {
-        margin: 0;
-        padding: 0;
-        font-family: 'Yellowtail', cursive;
-        box-sizing: border-box;
-    }
-    body {
-        background-color: ${(props) => props.theme.colors.primary};
-        color: ${(props) => props.theme.colors.white};
-    }
+  * {
+    margin: 0;
+    padding: 0;
+    font-family: 'Yellowtail', cursive;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: ${(props) => props.theme.colors.primary};
+    color: ${(props) => props.theme.colors.white};
+  }
+
+  input {
+    width: 360px;
+    height: 48px;
+    border-radius: 8px;
+    background-color: ${(props) => props.theme.colors.secondary};
+    font-size: 26px;
+  }
 `;
