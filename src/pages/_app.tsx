6dccf0f243f71512525a0737a7d2753554c53c62
Bruno <bruno_moises_stz@hotmail.com>
2023-04-24 15:12:00 -0300

    feat: create initial page to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import {ThemeProvider} from "styled-components";
+import {darkTheme} from "@/styles/themes/dark";
+import GlobalStyles from '../styles/global'
+import AppTemplate from "@/components/AppTemplate";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <ThemeProvider theme={darkTheme}>
+        <GlobalStyles />
+        <AppTemplate>
+          <Component {...pageProps} />
+        </AppTemplate>
+      </ThemeProvider>
+  );
 }
