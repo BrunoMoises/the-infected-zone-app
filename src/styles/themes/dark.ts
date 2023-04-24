bc9ee50319a08b425e0e383eb3e8886fe77cd756
Bruno <bruno_moises_stz@hotmail.com>
2023-04-24 15:11:34 -0300

    style: create dark theme

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,12 @@
+export const darkTheme = {
+    title: 'dark',
+    colors: {
+        primary: '#19172A',
+        secondary: '#3A3760',
+        tertiary: '#635BD6',
+        black1: '#131212',
+        black2: '#171616',
+        black3: '#1E1E1E',
+        white: '#fff'
+    }
+};
