a775d90ac7b977f8948d121821cc62d7f7bff1be
Bruno <bruno_moises_stz@hotmail.com>
2023-04-24 16:40:11 -0300

    style: create styles to home

---
+++ src/styles/home/home.styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 100px;
+  width: 100%;
+  height: 100%;
+`
+
+export const FilterContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+export const CardContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 47px;
+  position: absolute;
+  left: 0;
+`
