3c53be633ba32ae1762d72a401e5299a0d4859e2
Bruno <bruno_moises_stz@hotmail.com>
2023-04-28 09:34:03 -0300

    style: add container and style for scrollbar

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,10 +1,29 @@
-import {createGlobalStyle} from 'styled-components'
+import styled, {createGlobalStyle} from 'styled-components'
+
+export const Container = styled.div`
+  width: 90%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 60px;
+`
 
 export default createGlobalStyle`
+  body::-webkit-scrollbar {
+    width: 14px;               
+  }
+
+  body::-webkit-scrollbar-thumb {
+    background-color: ${props => props.theme.colors.secondary};
+    border-radius: 20px;
+  }
+  
   * {
     margin: 0;
     padding: 0;
-    font-family: 'Yellowtail', cursive;
+    font-family: 'Lobster', cursive;
     box-sizing: border-box;
   }
 
@@ -19,5 +38,29 @@ export default createGlobalStyle`
     border-radius: 8px;
     background-color: ${(props) => props.theme.colors.secondary};
     font-size: 26px;
+  }
+
+  button {
+    border-radius: 20px;
+    width: 185px;
+    height: 48px;
+    font-size: 24px;
+    background-color: ${(props) => props.theme.colors.tertiary};
+    border: none;
+    cursor: pointer;
+
+    :hover {
+      background-color: ${(props) => props.theme.colors.secondary};
+    }
+  }
+
+  h1 {
+    width: 100%;
+    position: absolute;
+    font-size: 45px;
+    top: 0;
+    text-align: center;
+    background-color: #201f3b;
+    padding: 20px;
   }
 `;
