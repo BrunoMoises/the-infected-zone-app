299ee762e5050dffed71ff506157be4d39b419ff
Bruno <bruno_moises_stz@hotmail.com>
2023-04-28 09:30:16 -0300

    style: create styles to "Sobre"

---
+++ src/styles/sobre/sobre.styles.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 90%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  margin-top: 140px;
+  margin-bottom: 140px;
+`
+
+export const Text = styled.div`
+  display: flex;
+  gap: 30px;
+  flex-direction: column;
+  font-size: 26px;
+  text-align: justify;
+  color: #B3B3B3;
+`
+export const Banner = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
+  img {
+    border-radius: 20px;
+  }
+`
